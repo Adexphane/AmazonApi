c522d5ea8dff5498649373574483a10c9aec29d1
Adexphane <adexphane@gmail.com>
2025-01-25 03:56:17 +0100

    Add Vercel config

--- index.js
+++ index.js
@@ -82,4 +82,6 @@ app.get("/products/:productId/offers", async (req, res) => {
     }
   });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(process.env.PORT || 5000, () => 
+    console.log(`Server ready on port ${process.env.PORT || 5000}`)
+  );
